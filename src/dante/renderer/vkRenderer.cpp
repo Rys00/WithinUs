b263a350b069c0e8a571932f00c7869c20a094a9
mogniews <mateusz.ogniewski@gmail.com>
2024-04-23 16:34:26 +0200

    GPUS??? :O

--- src/dante/renderer/vkRenderer.cpp
+++ src/dante/renderer/vkRenderer.cpp
@@ -1,17 +1,88 @@
 #include <vulkan/vulkan.h>
+// #ifdef WINDOWS_BUILD
+#include <vulkan/vulkan_win32.h>
+// #endif
+#include <iostream>
 
-VkResult vkInit() {
+#define ArraySize(arr) sizeof((arr))/sizeof((arr[0]))
+
+#define VK_CHECK(result)\
+if(result != VK_SUCCESS) {\
+  std::cout << "Vulkan error: " << result << std::endl;\
+  __debugbreak();\
+  return false;\
+}
+
+struct VkContext {
+  VkInstance instance;
+  VkSurfaceKHR surface;
+  int graphicsIdx;
+  VkPhysicalDevice gpu;
+};
+
+bool vkInit(VkContext* vkcontext, void* window) {
   VkApplicationInfo appInfo = {};
   appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
   appInfo.pApplicationName = "WithinUs";
   appInfo.pEngineName = "Dante";
 
+  char* extensions[] = {
+// #ifdef WINDOWS_BUILD
+    VK_KHR_WIN32_SURFACE_EXTENSION_NAME,
+// #endif
+    VK_KHR_SURFACE_EXTENSION_NAME
+  };
+
   VkInstanceCreateInfo instanceInfo = {};
   instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
   instanceInfo.pApplicationInfo = &appInfo;
+  instanceInfo.ppEnabledExtensionNames = extensions;
+  instanceInfo.enabledExtensionCount = ArraySize(extensions);
+  VK_CHECK(vkCreateInstance(&instanceInfo, 0, &vkcontext->instance));
 
-  VkInstance instance;
+  {
+// #ifdef WINDOWS_BUILD
+    VkWin32SurfaceCreateInfoKHR surfaceInfo = {};
+    surfaceInfo.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
+    surfaceInfo.hwnd = (HWND)window;
+    surfaceInfo.hinstance = GetModuleHandleA(0);
+    VK_CHECK(vkCreateWin32SurfaceKHR(vkcontext->instance, &surfaceInfo, 0, &vkcontext->surface));
+// #endif
+  }
 
-  VkResult result = vkCreateInstance(&instanceInfo, 0, &instance);
-  return result;
+  {
+    vkcontext->graphicsIdx = -1;
+    uint32_t gpuCount = 0;
+    VkPhysicalDevice gpus[10];
+    VK_CHECK(vkEnumeratePhysicalDevices(vkcontext->instance, &gpuCount, 0));
+    VK_CHECK(vkEnumeratePhysicalDevices(vkcontext->instance, &gpuCount, gpus));
+
+    for(uint32_t i = 0; i < gpuCount; ++i) {
+      VkPhysicalDevice gpu = gpus[i];
+      VkQueueFamilyProperties queueProps[10];
+      uint32_t queueFamilyCount = 0;
+      vkGetPhysicalDeviceQueueFamilyProperties(gpu, &queueFamilyCount, 0);
+      vkGetPhysicalDeviceQueueFamilyProperties(gpu, &queueFamilyCount, queueProps);
+
+      for(uint32_t j = 0; j < queueFamilyCount; ++j) {
+        if(queueProps[j].queueFlags && VK_QUEUE_GRAPHICS_BIT) {
+          VkBool32 surfaceSupport = VK_FALSE;
+          VK_CHECK(vkGetPhysicalDeviceSurfaceSupportKHR(gpu, j, vkcontext->surface, &surfaceSupport));
+
+          if(surfaceSupport) {
+            vkcontext->graphicsIdx = j;
+            vkcontext->gpu = gpu;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  if(vkcontext->graphicsIdx < 0) {
+    return false;
+  }
+
+
+  return true;
 }
--- src/platform/win32Platform.cpp
+++ src/platform/win32Platform.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <windows.h>
-
+#include "dante/renderer/vkRenderer.cpp"
 
 static bool running = true;
 
@@ -15,45 +15,61 @@ LRESULT CALLBACK platformWindowCallback(HWND window, UINT msg, WPARAM wParam, LP
 }
 
 
-bool platformCreateWindow(HWND window) {
+bool platformCreateWindow(HWND* window) {
   HINSTANCE instance = GetModuleHandleA(0);
 
   WNDCLASS wc = {};
   wc.lpfnWndProc = platformWindowCallback;
   wc.hInstance = instance;
   wc.lpszClassName = "vulkan_engine_class";
+  wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 
   if(!RegisterClassA(&wc)) {
-    MessageBoxA(window, "Failed registering window class", "error", MB_ICONEXCLAMATION |  MB_OK);
+    MessageBoxA(0, "Failed registering window class", "error", MB_ICONEXCLAMATION |  MB_OK);
     return false;
   }
 
-  window = CreateWindowExA(
+  *window = CreateWindowExA(
     WS_EX_APPWINDOW,
     "vulkan_engine_class",
     "WithinUs",
-    WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_OVERLAPPED,
+    WS_THICKFRAME | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_OVERLAPPED,
     100, 100, 600, 300, 0, 0, instance, 0
   );
 
   if(window == 0) {
-    MessageBoxA(window, "Failed creating window", "error", MB_ICONEXCLAMATION |  MB_OK);
+    MessageBoxA(0, "Failed creating window", "error", MB_ICONEXCLAMATION |  MB_OK);
     return false;
   }
 
-  ShowWindow(window, SW_SHOW);
+  ShowWindow(*window, SW_SHOW);
   return true;
 }
 
 
+void platformUpdateWindow(HWND window) {
+  MSG msg;
+
+  while(PeekMessage(&msg, window, 0, 0, PM_REMOVE)) { 
+    TranslateMessage(&msg);
+    DispatchMessageA(&msg);
+  }
+}
+
+
 int main() {
+  VkContext vkcontext = {};
   HWND window = 0;
-  if(!platformCreateWindow(window)) {
+  if(!platformCreateWindow(&window)) {
+    return -1;
+  }
+
+  if(!vkInit(&vkcontext, &window)) {
     return -1;
   }
 
   while(running) {
-
+    platformUpdateWindow(window);
   }
   return 0;
 }
